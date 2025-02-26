84800145a459cfe072b76663ad3df2d1aca97b3b
Darling Selita <darling.selita21@umt.edu.al>
2025-02-26 18:45:04 +0100

    Second Commit, Account Controller Done

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using EliteTGTask.Models;
 using System.Linq.Expressions;
+using Microsoft.AspNetCore.Server.HttpSys;
+using Amazon.S3;
 //using EliteTGTask.ViewModels;
 
 namespace EliteTGTask.Controllers
@@ -21,13 +23,14 @@ namespace EliteTGTask.Controllers
         //Ketu eshte Sign Up-i, vetem bejme retrieve View
         [HttpGet]
 
-        public IActionResult Signup()
+        public ViewResult Signup()
         {
             return View();
         }
 
-        // Ketu bejme POST ose dergojme te dhenat per krijimin e nje acc te ri (breakpoint tek line i krijimit te user per te pare nese vjen gje apo jo)
+        // Ketu behet POST ose dergojm te dhenat per krijimin e nje acc te ri (breakpoint tek line i krijimit te user per te pare nese vjen gje apo jo)
         [HttpPost]
+        [ActionName("Signup")]
         public async Task<IActionResult> Signup(SignupViewModel Model)
         {
             if (ModelState.IsValid)
@@ -52,20 +55,35 @@ namespace EliteTGTask.Controllers
 
         [HttpGet]
 
-        public IActionResult Login()
+        public ViewResult Login()
         {
             return View();
         }
 
         [HttpPost]
+        [ActionName("Login")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginViewModel Model)
         {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Model.Username, Model.Password, _userManager);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid Loign Attempt");
             return View(Model);
         }
 
-        // 
+        // Log Out
 
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
 
         public IActionResult Index()
         {
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,43 @@
 using System.Diagnostics;
-using EliteTGTask.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EliteTGTask.Models;
 
 namespace EliteTGTask.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDBContext dbContext)
         {
-            _logger = logger;
+            _dbContext = dbContext;
         }
+
+        // Blog Index
+        public async Task<IActionResult> Index(string SearchKeyword, int categoryId = 0, int page = 1)
+        {
+            var postsQuery = _dbContext.Posts
+                .Include(p => p.User)
+                .Include(p => p.PostCategories)
+                .ThenInclude(pc => pc.Category)
+                .OrderByDescending(p => p.CreatedAt)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(SearchKeyword))
+            {
+                postsQuery = postsQuery.Where(p => p.Title.Contains(SearchKeyword) || p.Description.Contains(SearchKeyword));
+            }
+
+            if (categoryId > 0)
+            {
+                postsQuery = postsQuery.Where(p => p.PostCategories.Any(pc => pc.CategoryId == categoryId));
+            }
+
+
+            return View();
+        }
+
 
         public IActionResult Index()
         {
